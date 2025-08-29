29bd0a23fb0dd9d2bd7be37ef314eae9be9eeee0
BrajamA <brajam.arcila.ceuz@gmail.com>
2025-08-29 11:09:09 -0500

    Clase2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,18 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hola a todos");
+
+        System.out.println("COMANDOS GIT");
+        System.out.println("git status- estado rama");
+        System.out.println("git branch - ver las ramas");
+        System.out.println("git add - agrupar archivos");
+        System.out.println("git push origin - subir cambios");
+        System.out.println("git commit -m guardar cambios local");
+        System.out.println("git config -- list - listar configuracion");
+        System.out.println("git log - ver todos los commit");
+        System.out.println("git clone - clonar proyecto");
+        System.out.println("git fetch - ver ramas remotas");
+        System.out.println("git switch - para crear y posicionarme sobre la rama");
     }
 }
